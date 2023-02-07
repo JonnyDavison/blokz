fd7b0aa78ead38b378da7184136ddb36266c6208
JonnyDavison <jonnydavison@icloud.com>
2023-02-07 13:04:50 +0000

    Edit "makeBrick" add for loop for achor position

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,17 +15,17 @@ class brick {
 let bricks = [
     new brick(10, 280)
 ]
-console.log(bricks[0])
+
 
 //Draw brick
-// function makeBrick() {
-//     for (let i=0; i < bricks.length; i++ ) {
-//         let brick = document.createElement('div');
-//         brick.classList.add('brick')
-//         brick.style.left = bricks[i].bottomLeft[0]
-//         brick.style.bottom = '50px'
-//         grid.appendChild(brick)
-//     }
-// }
+function makeBrick() {
+    for (let i=0; i < bricks.length; i++ ) {
+        let brick = document.createElement('div');
+        brick.classList.add('brick')
+        brick.style.left = bricks[i].bottomLeft[0] + 'px'
+        brick.style.bottom = bricks[i].bottomLeft[1] + 'px'
+        grid.appendChild(brick)
+    }
+}
 
-// makeBrick()
+makeBrick()
