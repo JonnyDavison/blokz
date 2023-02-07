31dcd85989cd746535bece5244fbd8821782915b
JonnyDavison <jonnydavison@icloud.com>
2023-02-07 12:36:47 +0000

    Create brick class

---
+++ assets/js/script.js
@@ -0,0 +1,25 @@
+let grid = document.querySelector('.grid')
+const brickWidth = '100px'
+const brickHeight = '20px'
+
+// Create Brick
+class brick {
+    constructor(xAxis, yAxis){
+        this.bottomLeft = [xAxis, yAxis]
+        this.bottomRight = [xAxis + brickWidth, yAxis]
+        this.topLeft = [xAxis, yAxis + brickHeight]
+        this.topRight = [xAxis + brickHeight, yAxis + brickHeight]
+    }
+}
+
+//Draw brick
+ 
+function makeBrick() {
+    let brick = document.createElement('div');
+    brick.classList.add('brick')
+    brick.style.left = '50px'
+    brick.style.bottom = '50px'
+    grid.appendChild(brick)
+}
+
+makeBrick()
