b1ca2b0de8eff67b08e3efdf93d076272294ee82
JonnyDavison <jonnydavison@icloud.com>
2023-04-12 12:39:03 +0000

    Add detailed commenting to script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,18 @@
-// Variables
+// Global Variables
 // Grid
 let grid = document.querySelector('.grid');
 let gridWidth = 560;
 let gridHeight = 300;
 
-// Create Brick
+// Create Brick Dimensions
 let brickWidth = 100;
 let brickHeight = 20;
 
-// Slider Starting point
+// Slider Starting point, (x,y) coordinates for the slider starting point
 let sliderStart = [230, 10];
 let sliderPosition = sliderStart;
 
-// Ball
+// Ball Diameter 
 let ballWidth = 15;
 
 // Ball starting point
@@ -26,7 +26,7 @@ let yMove = 2;
 //Timer
 let ballSpeed;
 
-// Score Board
+// Score Board get the score value
 let scoreBoard = document.querySelector('#score');
 let score = 0;
 
@@ -42,7 +42,7 @@ class brick {
 }
 
 
-// Make multiple bricks
+// Make multiple bricks 
 let bricks = [
     new brick(10, 280),
     new brick(120, 280),
@@ -62,19 +62,23 @@ let bricks = [
 ];
 
 
-//Draw brick
+// Draw bricks, iterates through the bricks and creates a new div for each coordinate
 function makeBrick() {
     for (let i = 0; i < bricks.length; i++) {
+        // Creates new div
         let brick = document.createElement('div');
+        // Gives the brick the class of 'bricks'
         brick.classList.add('brick');
+        // Provides each new brick with X,Y coordinate
         brick.style.left = bricks[i].bottomLeft[0] + 'px';
         brick.style.bottom = bricks[i].bottomLeft[1] + 'px';
+        // Adds brick the the grid in location provided
         grid.appendChild(brick);
     }
 }
 
 
-// Calls Make brick when screen size is above 650px 
+// Calls MakeBrick() when screen size is above 650px 
 function callMakeBrick(large) {
     if (large.matches) {
         makeBrick();
@@ -86,21 +90,21 @@ callMakeBrick(large);
 large.addListener(callMakeBrick);
 
 
-// Create slider
+// Create slider as a div
 let slider = document.createElement('div');
 slider.classList.add('slider');
 makeSlider();
 grid.appendChild(slider);
 
 
-// Make Slider
+// Places slider on the grid with (x,y) style coordinates
 function makeSlider() {
     slider.style.left = sliderStart[0] + 'px';
     slider.style.bottom = sliderStart[1] + 'px';
 }
 
 
-//Slider movement 
+// Slider movement, keyboard input event calls the makeSlider function
 function moveSlider(event) {
     switch (event.key) {
         case 'ArrowLeft':
@@ -122,21 +126,21 @@ function moveSlider(event) {
 document.addEventListener('keydown', moveSlider);
 
 
-// Make Ball
+// Make Ball creates the ball in position
 function makeBall() {
     ball.style.left = ballPosition[0] + 'px';
     ball.style.bottom = ballPosition[1] + 'px';
 }
 
 
-// Create ball
+// Create ball as a div on the grid
 let ball = document.createElement('div');
 ball.classList.add('ball');
 makeBall();
 grid.appendChild(ball);
 
 
-//Ball movement
+//Ball movement, gives ball direction by adding to the (x, y) coordinates
 function moveBall() {
     ballPosition[0] += xMove;
     ballPosition[1] += yMove;
@@ -145,18 +149,18 @@ function moveBall() {
 }
 
 
-// Ball Bounce
+// Ball Bounce gives teh ball the ability to bouce off objects
 function changeDirection() {
-    // Wall Bounce
+    // Wall Bounce creates the height and width limits for the ball to bounce off
     if (ballPosition[1] >= (gridHeight - ballWidth) || ballPosition[0] >= (gridWidth - ballWidth) || (ballPosition[0] <= 0)) {
         bounceBall();
     }
 
-    // Brick Bounce
+    // Brick Bounce iterates through the bricks and if the ball collides the ball bounces
     for (let i = 0; i < bricks.length; i++) {
         if ((ballPosition[0] > bricks[i].bottomLeft[0] && ballPosition[0] < bricks[i].bottomRight[0]) &&
             (ballPosition[1] > bricks[i].bottomLeft[1] && ballPosition[1] < bricks[i].topLeft[1])) {
-
+            // Removes the brick from the screen if the ball has collided with the brick   
             let groupBricks = document.querySelectorAll('.brick');
             groupBricks[i].classList.remove('brick');
             bricks.splice(i, 1);
@@ -164,7 +168,7 @@ function changeDirection() {
             score++;
             scoreBoard.innerHTML = score;
 
-            // Winner
+            // Winner, when all the bricks have been hit "WINNER" is displayed and the ball stops and the slider cannot be moved
             if (bricks.length === 0) {
                 scoreBoard.innerHTML = "WINNER!";
                 clearInterval(ballSpeed);
@@ -175,13 +179,13 @@ function changeDirection() {
         }
     }
 
-    // Slider Bounce
+    // Slider Bounce makes teh ball bounce back off the slider/player back up to the bricks
     if ((ballPosition[0] > sliderPosition[0] && ballPosition[0] < sliderPosition[0] + brickWidth) &&
         (ballPosition[1] > sliderPosition[1] && ballPosition[1] < sliderPosition[1] + brickHeight)) {
         bounceBall();
     }
 
-    // Game Over
+    // Game Over, if the ball goes below the slider "GAME OVER" is displayed and the ball stops and the slider cannot be moved
     if (ballPosition[1] <= 0) {
         clearInterval(ballSpeed);
         document.removeEventListener('keydown', moveSlider);
@@ -190,7 +194,7 @@ function changeDirection() {
     }
 }
 
-// Bounce direction
+// Bounce direction sets the rules for the direction ball will bounce after collision
 function bounceBall() {
     if (xMove === 2 && yMove === 2) {
         yMove = -2;
@@ -209,37 +213,42 @@ function bounceBall() {
         return;
     }
 }
-// Start ball movement and speed
+
+
+// Start Game 
 function startGame() {
+    // Starts the ball moving and sets teh speed
     ballSpeed = setInterval(moveBall, 30);
+    // Listens fo the key press
     document.addEventListener('keydown', moveSlider);
+    // Makes the start button disappear
     startButtonDisappear();
+    // Makes the Reset button appear
     resetButtonAppear();
+    // Calls the function to start the ball 
     changeDirection();
 }
 
-// Start button dissappear after use
+// Start button dissappear, called by start game
 function startButtonDisappear() {
     let startDiv = document.getElementById('start');
     startDiv.style.display = 'none';
 }
 
-
-// Reset butoon fuction
-function resetGame() {
-    window.location.reload();
-}
-
-
-// Reset button dissappear after use
+// Reset button appear, called by start game
 function resetButtonAppear() {
     let resetDiv = document.getElementById('reset');
     resetDiv.style.display = 'flex';
 }
 
+// Reset button function 
+function resetGame() {
+    window.location.reload();
+}
+
 
 //Touch screen button control 
-// Left arrow
+// Left arrow called by the left arrow button in the HTML
 function moveLeft() {
     let moveL = document.getElementById('slider');
     if (sliderStart[0] > 0) {
@@ -247,7 +256,7 @@ function moveLeft() {
         makeSlider();
     }
 }
-// Right arrow
+// Right arrow alled by the right arrow button in the HTML
 function moveRight() {
     const moveR = document.getElementById('slider');
     if (sliderStart[0] < gridWidth - brickWidth) {
@@ -257,36 +266,40 @@ function moveRight() {
 }
 
 
+
+// For small screens below 650px
+
 // Small screen function to resize
 function smallScreen(x) {
 
     if (x.matches) {
-        // Resize game area
+        // Downsize game area to fit smaller screen width
         document.getElementById('game-area').style.width = '310px';
         document.getElementById('game-area').style.height = '350px';
 
         // Re-declaire variable values 
-        // Grid
+        // Downsize the grid to fit smaller screens
         grid = document.querySelector('.grid');
         gridWidth = 280;
         gridHeight = 200;
 
-        // Create Brick
+        // Create Brick to smaller size to fit reduced grid dimensions
         brickWidth = 50;
         brickHeight = 15;
 
-        // Slider Starting point
+        // Slider Starting point to center in smaller grid
         sliderStart = [125, 10];
         sliderPosition = sliderStart;
 
-        // Ball diameter 
+        // Ball diameter remains same for visibility
         ballWidth = 15;
 
-        // Ball starting point
+        // Ball starting point to center in smaller grid
         ballStart = [140, 30];
         ballPosition = ballStart;
 
-        // X/Y co-ordinates for brick positioning on smaller grid
+        // X/Y co-ordinates for brick positioning in smaller grid 
+        // Allows the smaller bricks to be dispersed equally within reduced grid size 
         bricks = [
             new brick(5, 180),
             new brick(60, 180),
@@ -306,47 +319,57 @@ function smallScreen(x) {
         ];
 
 
-        // Create smaller bicks in new location
+        // Create smaller bicks in new location iterates through the bricks and creates a new div for each coordinate
         let smallBrick = function () {
             for (let i = 0; i < bricks.length; i++) {
+                // Creates new div
                 let brick = document.createElement('div');
+                // Gives the brick the class of 'bricks'
                 brick.classList.add('brick');
+                // Provides each new brick with X,Y coordinate
                 brick.style.left = bricks[i].bottomLeft[0] + 'px';
                 brick.style.bottom = bricks[i].bottomLeft[1] + 'px';
+                // Adds brick to the grid in location provided
                 grid.appendChild(brick);
             }
         };
 
-
-        // Create slider in new position 
+        // Create slider in new position by +/- starting point for the slider
         let makeSmallSlider = function () {
             slider.style.left = sliderStart[0] + 'px';
             slider.style.bottom = sliderStart[1] + 'px';
         };
 
 
-        // Controls for slider in smaller grid 
+        // Keyboard controls for slider in smaller grid 
         let smallSlider = function (event) {
             if (this.item === undefined) {
                 return;
             }
+            // When arrow left key input detected moves the slider 
             switch (event.key) {
                 case 'ArrowLeft':
+                    // prevents slider from leaving the grid
                     if (sliderStart[0] > 0) {
+                        // moves slider 10px to the left from current position
                         sliderStart[0] -= 10;
+                        // calls Make sider with new x axis starting point
                         makeSmallSlider();
                     }
                     break;
-
+                // When arrow right key input detected moves the slider         
                 case 'ArrowRight':
+                    // prevents slider from leaving the grid
                     if (sliderStart[0] < gridWidth - brickWidth) {
+                        // moves slider 10px to the right from current position
                         sliderStart[0] += 10;
+                        // calls Make sider with new x axis starting point
                         makeSmallSlider();
                     }
                     break;
             }
         };
-        // Callin functions
+        // Calling functions for the small screen size
         smallBrick();
         makeSmallSlider();
         makeBall();
